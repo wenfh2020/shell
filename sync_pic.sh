a6b3af08a02817f37f0da192ff305009fca2ed9b
wenfh2020 <wenfh2020@126.com>
2019-12-13 09:35:20 +0800

    优化脚本

--- sync_pic.sh
+++ sync_pic.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 # wenfahua/2019/12/13
 
-datename=$(date +%Y%m%d%H%M%S)
-src_dir_path='/Users/wenfahua/Library/Application Support/typora-user-images'
+work_path=`dirname $0`
+cd $work_path
+work_path=`pwd`
+echo $work_path
+
+# typora app 存图片的默认路径
+src_pic_path='/Users/wenfahua/Library/Application Support/typora-user-images'
 
 # 修改前进行数据备份。
-cp -r ../typora ../typora.$datename
+datename=$(date +%Y%m%d%H%M%S)
+cp -r $work_path $work_path.$datename
 
-# 拷贝图片到 pic 文件夹下。
-infos=`grep -r $src_dir_path --include '*.md' . | tr " " "\?"`
+# 查找有图片本地路径的文件，拷贝图片到 pic 文件夹下。
+infos=`grep -r $src_pic_path --include '*.md' . | tr " " "\?"`
+# echo $infos
 for info in $infos
 do
     echo $info
@@ -30,11 +37,11 @@ do
     fi
 done
 
-# 替换文件路径字符串，因为一个文件可能有多个字符串，所以要 uniq
-files=`grep -r $src_dir_path --include '*.md' . | tr " " "\?"| awk -F ':' '{print $1}'|uniq`
+# 替换图片本地路径字符串为相对路径，因为一个文件可能有多个字符串，所以要 uniq
+files=`grep -r $src_pic_path --include '*.md' . | tr " " "\?"| awk -F ':' '{print $1}'|uniq`
 for file in $files
 do
-    sed -i '' "s:$src_dir_path:\.\/pic:g" $file
+    sed -i '' "s:$src_pic_path:\.\/pic:g" $file
     if [ $? -eq 0 ]; then
         echo "replace success $file"
     fi
